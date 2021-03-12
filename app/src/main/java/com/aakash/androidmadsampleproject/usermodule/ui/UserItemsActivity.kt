f20ce6f5511f1fe6ce25fbc6fa000858abe78986
akashmehta <akashmehta4444@gmail.com>
2021-03-12 08:20:54 +0530

    Refactored logic. Made general viewmodel factory for all viewmodels.

--- app/src/main/java/com/aakash/androidmadsampleproject/usermodule/ui/UserItemsActivity.kt
+++ app/src/main/java/com/aakash/androidmadsampleproject/usermodule/ui/UserItemsActivity.kt
@@ -1,7 +1,6 @@
 package com.aakash.androidmadsampleproject.usermodule.ui
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -9,16 +8,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.aakash.androidmadsampleproject.AppApplication
 import com.aakash.androidmadsampleproject.R
 import com.aakash.androidmadsampleproject.commonconfig.shareddata.endpoint.ApiEndPoint
-import com.aakash.androidmadsampleproject.usermodule.dto.UserItemResponse
 import com.aakash.androidmadsampleproject.usermodule.model.UserItemsViewModel
-import com.aakash.androidmadsampleproject.usermodule.model.UserViewModelFactory
 import kotlinx.android.synthetic.main.activity_user_items.*
 import javax.inject.Inject
 
 class UserItemsActivity : AppCompatActivity(){
 
     private lateinit var userViewModel: UserItemsViewModel
-    private lateinit var userViewModelFactory: UserViewModelFactory
 
     @Inject
     lateinit var apiService: ApiEndPoint
@@ -26,8 +22,7 @@ class UserItemsActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_items)
         (application as AppApplication).appComponent.inject(this)
-        userViewModelFactory = UserViewModelFactory(apiService)
-        userViewModel = ViewModelProvider(this, userViewModelFactory)[UserItemsViewModel::class.java]
+        userViewModel = ViewModelProvider(this, UserItemsViewModel.FACTORY(apiService))[UserItemsViewModel::class.java]
         setupApi()
     }
 
