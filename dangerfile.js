a0142dbeb4daa1d32143f8b35f47451bf46dd173
akashmehta <akashmehta4444@gmail.com>
2021-03-18 19:06:48 +0530

    Added danger

---
+++ dangerfile.js
@@ -0,0 +1,9 @@
+import {fail, warn, message, markdown, danger} from "danger"
+
+fail("this is failure message")
+warn("this is warning")
+message("This is normal message")
+markdown("*Markdown* is also **Supported**")
+
+const {additions = 0, deletions = 0} = danger.github.pr
+message (`:tada: The PR added ${additions} and removed ${deletions} lines.`)
