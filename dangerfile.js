37e0371e5637f871e93093d8206df2e0f5428603
akashmehta <akashmehta4444@gmail.com>
2021-03-18 19:52:27 +0530

    Updated rule

--- dangerfile.js
+++ dangerfile.js
@@ -7,3 +7,7 @@ markdown("*Markdown* is also **Supported**")
 
 const {additions = 0, deletions = 0} = danger.github.pr
 message (`:tada: The PR added ${additions} and removed ${deletions} lines.`)
+
+if !(github.pr_body + github.pr_title).include?("AMS")
+    warn("Please enter valid message")
+end
